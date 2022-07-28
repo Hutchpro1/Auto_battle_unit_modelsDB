1bcc54714d3b60961fa8a777934052f162f14cab
Jonathan <MonoToastJLR@gmail.com>
2022-07-28 10:26:00 -0500

    Add Nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
+import Game from './pages/Game';
+import LeaderBoard from './pages/LeaderBoard';
+import UnitData from './pages/UnitData';
+import Header from './componets/Header';
+import { render } from '@testing-library/react';
 
 function App() {
+
+  let userName
+
+  let pageState = 'unit'
+  let page
+
+  updatePage(pageState)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header  State={pageState} Update={updatePage} />
+      {page}
     </div>
   );
+
+  function updatePage(state) {
+    if(state === 'login') {
+      page = <Login State={pageState} LoginName={userName} />
+      console.log('loginPage')
+    }
+    else if (state === 'game') {
+      page = <Game State={pageState} LoginName={userName} />
+      console.log('gamePage')
+    }
+    else if (state === 'leader') {
+      page = <LeaderBoard State={pageState} LoginName={userName} />
+      console.log('leaderPage')
+    }
+    else if (state === 'unit') {
+      page = <UnitData State={pageState} LoginName={userName} />
+      console.log('unitPage')
+    }
+  }
 }
 
 export default App;
